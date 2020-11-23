c6de27045e26f65dd165773504b325e6fce3d9b4
Nicolas LAndel <nicolas.landel@hotmail.com>
2020-11-23 09:51:54 +0100

    Use next method

--- application/backend/app.js
+++ application/backend/app.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    res.json({message: '1ere requete'});
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
     res.json({message: 'votre requete a bien été reçue'});
 });
 
