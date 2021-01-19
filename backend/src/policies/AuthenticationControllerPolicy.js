37de925504542c74fb936f724caa76c7e0fdd6b5
Nicolas LAndel <nicolas.landel@hotmail.com>
2021-01-19 10:14:53 +0100

    Set the authentication policy

--- backend/src/policies/AuthenticationControllerPolicy.js
+++ backend/src/policies/AuthenticationControllerPolicy.js
@@ -6,9 +6,31 @@ module.exports = {
         const schema = {
             email: Joi.string().email(),
             password: Joi.string().regex(
-                new RegExp('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$')
+                new RegExp('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,50}$')
             )
         }
-        next()
+
+        const {error, value} = Joi.validate(req.body, schema)
+
+        if (error) {
+            switch (error.details[0].context.key) {
+                case 'email':
+                    res.status(400).send({
+                        error: 'You must provide a valid email address'
+                    })
+                    break
+                case 'password':
+                    res.status(400).send({
+                        error: `The password must contain at least 8 characters with at least one letter and one numeric character`
+                    })
+                    break
+                default:
+                    res.statuc(400).send({
+                        error: 'Invalid registration information'
+                    })
+            }
+        } else {
+            next()
+        }
     }
 }
