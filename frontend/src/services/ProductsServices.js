e27b42107358babbf98f52b21d329873411568b3
Nicolas LAndel <nicolas.landel@hotmail.com>
2021-02-02 15:17:39 +0100

    Comment unused code

--- frontend/src/services/ProductsServices.js
+++ frontend/src/services/ProductsServices.js
@@ -1,13 +1,13 @@
-import Api from '@/services/Api'
+// import Api from '@/services/Api'
 
-export default {
-  index (search) {
-    return Api().get('products', {
-      params: {
-        search: search
-      }
-    })
-  }
+// export default {
+//   index (search) {
+//     return Api().get('todos', {
+//       params: {
+//         search: search
+//       }
+//     })
+//   }
 //   show (productId) {
 //     return Api().get(`products/${productId}`)
 //   },
