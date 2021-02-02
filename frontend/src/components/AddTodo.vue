1e3ded528da78f64fd00a35fef719d25d0ac21e5
Nicolas LAndel <nicolas.landel@hotmail.com>
2021-02-02 15:50:42 +0100

    Work on vue

---
+++ frontend/src/components/AddTodo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form class="add-todo-form centered-form" @submit="addTodo">
+      <input type="text" v-model="title"  name="title" placeholder="Add todo">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+  data () {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo (e) {
+      e.preventDefault()
+      const newTodo = {
+        title: this.title, // Use 'this' because we said v-model="title" in the DOM
+        completed: false
+      }
+      this.$emit('add-todo', newTodo)
+
+      this.title = ''
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .header {
+    background-color: rgb(58, 65, 65);
+    color: rgb(236, 230, 219);
+  }
+</style>
